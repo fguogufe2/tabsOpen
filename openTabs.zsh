23f8c1fd64e74b92a9af30c20410413dad50a8a3
fguogufe2 <fguogufe2@gmail.com>
2024-01-10 15:25:11 -0800

    c

---
+++ openTabs.zsh
@@ -0,0 +1,19 @@
+#! /usr/bin/env zsh
+
+
+# list all webpage url in double quotes, separated by spaces:
+
+webPages=(
+    "https://www.h-net.org/jobs/job_browse.php?category_id=104"
+    
+)
+
+
+#  open all pages in safari:
+
+for i in $webPages; do
+    open "$i"
+done
+
+
+printf "------All webpages opened---------\n"
